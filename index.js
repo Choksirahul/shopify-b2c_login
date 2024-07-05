95e54dbb512ac281475bf628180a36141e72574b
Rahul Choksi <rahul.choksi@1rivet.com>
2024-07-05 15:04:57 +0530

    removed cors header from client-side

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const cors = require("cors");
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
